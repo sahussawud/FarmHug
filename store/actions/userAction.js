39c9bfad5876b6508147a15f49ba219d7a96e6a3
sahussawud <sahussawud@hotmail.com>
2020-11-20 10:48:38 +0700

    make authentication reducer

--- store/actions/userAction.js
+++ store/actions/userAction.js
@@ -1,10 +1,34 @@
 export const PROFILE_UPDATE = "PROFILE_UPDATE"
+export const RESTORE_TOKEN = "RESTORE_TOKEN"
+export const SIGN_IN = "SIGN_IN"
+export const SIGN_OUT = "SIGN_OUT"
+
 
 
 export const profile_update = (profile) =>{
     return {
         type: PROFILE_UPDATE,
         profile: profile
+    }
+}
+
+export const restore_token = (token) =>{
+    return {
+        type: RESTORE_TOKEN,
+        token: token
+    }
+}
+
+export const sign_in = (token) =>{
+    return {
+        type: SIGN_IN,
+        token: token
+    }
+}
+
+export const sign_out = () =>{
+    return {
+        type: SIGN_OUT
     }
 }
 
--- store/reducers/userReducer.js
+++ store/reducers/userReducer.js
@@ -1,5 +1,5 @@
 // import User from '../../models/user';
-import {USER} from '../../data/data-dummy'
+import { USER } from '../../data/data-dummy'
 import { PROFILE_UPDATE } from '../actions/userAction'
 
 const initialUser = USER
@@ -7,6 +7,11 @@ const initialUser = USER
 
 const initialState = {
     profile: initialUser,
+    authentication: {
+        isLoading: true,
+        isSignout: false,
+        userToken: null,
+    }
 
 }
 
@@ -18,6 +23,35 @@ const userReducer = (state = initialState, action) => {
                 ...state,
                 profile: action.profile
             }
+        case 'RESTORE_TOKEN':
+            return {
+                ...state,
+                authentication:{
+                    ...state.authentication,
+                    userToken: action.token,
+                    isLoading: false,
+                }
+                
+            };
+        case 'SIGN_IN':
+            return {
+                ...state,
+                authentication:{
+                    ...state.authentication,
+                    isSignout: false,
+                    userToken: action.token,
+                }
+               
+            };
+        case 'SIGN_OUT':
+            return {
+                ...state,
+                authentication:{
+                    ...state.authentication,
+                    isSignout: true,
+                    userToken: null,
+                }
+            };
         default: 
             return state;
     }
